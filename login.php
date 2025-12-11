9057b70c7bd6a472b1ad506debdd48c40118d311
jara22222 <jarajoaquin750@gmail.com>
2025-12-11 13:29:34 +0800

    Updated project files

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include 'db.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username='$username'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+
+    if (password_verify($password, $row['password'])) {
+       
+        header("Location: message.html");
+    } else {
+        echo "Invalid password!";
+    }
+} else {
+    echo "No user found!";
+}
+?>
